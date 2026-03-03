a24c155932d0beb4f59b8b0d7e4aafa8e1397ff7
rachanaramkumar123 <rr5711@srmist.edu.in>
2026-03-03 10:59:12 +0530

    Rachana Added UC4-PalindromeStack

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,32 +1,42 @@
-public class UseCase4PalindromeCheckerApp {
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class UseCase6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         // Original String
         String original = "madam";
 
-        // Convert string to character array
-        char[] characters = original.toCharArray();
+        // Create Stack (LIFO)
+        Stack<Character> stack = new Stack<>();
 
-        // Two-pointer approach
-        int start = 0;
-        int end = characters.length - 1;
+        // Create Queue (FIFO)
+        Queue<Character> queue = new LinkedList<>();
+
+        // Push into stack and enqueue into queue
+        for (int i = 0; i < original.length(); i++) {
+            char ch = original.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
+        }
 
         boolean isPalindrome = true;
 
-        while (start < end) {
+        // Compare dequeue (FIFO) with pop (LIFO)
+        while (!stack.isEmpty()) {
 
-            // Compare start and end characters
-            if (characters[start] != characters[end]) {
+            char fromStack = stack.pop();      // Removes from end
+            char fromQueue = queue.remove();   // Removes from front
+
+            if (fromStack != fromQueue) {
                 isPalindrome = false;
                 break;
             }
-
-            start++;
-            end--;
         }
 
-        // Display result
+        // Print result
         if (isPalindrome) {
             System.out.println(original + " is a Palindrome");
         } else {
